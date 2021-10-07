1fa7d2727a4d26f583569a57ebfa9b5b5d3cdbd1
Ankan Datta <ml.engineer160119@gmail.com>
2021-10-07 19:09:45 +0530

    stage 3 make the get_data.py file

---
+++ README.md
@@ -0,0 +1,6 @@
+1. conda activate boston
+2. code -r requirements.txt
+3. pip install -r requirements.txt
+4. pip install -r requirements.txt
+5. code -r README.md
+6. 
--- template.py
+++ template.py
@@ -12,6 +12,7 @@ for dir_ in dirs:
     os.makedirs(dir_, exist_ok= True)
     with open(os.path.join(dir_, ".gitkeep"), "w") as f:
         pass
+    
 files = ["dvc.yaml",
           "params.yaml",
           ".gitignore",
