ddf063be2bd617409e8a62cd78218741fba09c42
Ankan Datta <ml.engineer160119@gmail.com>
2021-10-08 01:52:33 +0530

    workflow

--- app.py
+++ app.py
@@ -27,17 +27,16 @@ app = Flask(__name__,template_folder=template_dir)
 @app.route("/")
 @app.route("/index")
 
-
-
 def index():
 	return flask.render_template('index.html')
+
 
 @app.route("/predict",methods = ['POST'])
 def make_predictions():
 
     config = read_params(config_path=params_path)
     model_dir = config['webapp_model_dir']
-    
+
     with open(model_dir, 'rb') as f:
         reg = pickle.load(f)
 
@@ -58,8 +57,11 @@ def make_predictions():
             
             X = np.array([[a,c,d,e,f,g,h,i,j,k,l,m]])
             pred = abs(reg.predict(X))
+            pred = pred[0]
+            pred = round(pred, 3)
+            pred = str(pred)
             print(pred)
-            return flask.render_template('predict.html', response = pred[0])
+            return flask.render_template('predict.html', response = pred)
 
         except Exception as e:
             print(e)
