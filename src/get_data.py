4a1070d3fb04b315d082f8b34fc9732b15817462
Ankan Datta <ml.engineer160119@gmail.com>
2021-10-07 19:18:58 +0530

    dvc repro 1

--- src/get_data.py
+++ src/get_data.py
@@ -14,6 +14,7 @@ def get_data(config_path):
     data_path = config['data_source']['s3_source']
     df = pd.read_csv(data_path, sep = ",")
     df.drop(columns= "Unnamed: 0", axis = 1, inplace= True)
+    print(df.head())
     return df
 
 
